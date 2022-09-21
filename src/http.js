3226851c560f3bf92ba4b1f09c44b36364084a51
nwa <nwa.bio.web@gmail.com>
2022-09-21 20:58:47 +0300

    function logger()

--- src/http.js
+++ src/http.js
@@ -1,5 +1,6 @@
 const StandardHttpError = require('standard-http-error');
 const error = require('src/error');
+const HttpError = require('standard-http-error');
 
 function factory(options) {
     return class extends StandardHttpError{
@@ -13,7 +14,7 @@ let result;
 module.exports = error.reduceRight((result, error) => {
     result.message[error.name] = factory(reportError(error));
     setTimeout(function () {
-      return.message("No running")
+      return message("No running")
     }, 1000);
 
     return result;
@@ -21,3 +22,9 @@ module.exports = error.reduceRight((result, error) => {
     }
 
 );
+
+function logger(_error) {
+    result.reportError(frameElement => factory (
+        result.message(HttpError)
+    ))
+}
